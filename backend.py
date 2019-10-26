fac653c18db3fdb22553857596e4dd415764c03b
cjpontikes <cjpontikes@gmail.com>
2019-10-26 16:57:08 -0600

    got the login function working

--- backend.py
+++ backend.py
@@ -1,7 +1,23 @@
 import sqlite3
+global conn, c
 conn = sqlite3.connect('./miniproject1.db')
-
+c = conn.cursor()
+c.execute(' PRAGMA foreign_keys=ON ')
 
 
 def login(username, password):
-    pass
+    c.execute('SELECT uid, pwd FROM users;')
+    rows = c.fetchall()
+
+    for tuple in rows:
+        if(tuple[0] == username and tuple[1] == password):
+            return 1
+    return 0
+
+def main():
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
