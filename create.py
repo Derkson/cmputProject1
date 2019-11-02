cf5c14542a01753484a2c4cfaad9f706a9338ce8
cjpontikes <cjpontikes@gmail.com>
2019-11-02 12:45:15 -0600

    editing create statements

--- create.py
+++ create.py
@@ -27,27 +27,22 @@ def make_regno(table):
 def create_person(fname, lname, bdate, bplace, address, phone):
     insertions = (fname, lname, bdate, bplace, address, phone)
     c.execute('INSERT INTO persons VALUES(?,?,?,?,?,?);',insertions)
-    conn.commit()
 
 def create_birth(regno, fname, lname, regdate, regplace, gender, f_fname, f_lname, m_fname, m_lname):
     insertions = (regno, fname, lname, regdate, regplace, gender, f_fname,f_lname, m_fname, m_lname)
     c.execute('INSERT INTO births VALUES(?,?,?,?,?,?,?,?,?,?);', insertions)
-    conn.commit()
 
 def create_marriage(regno, regdate, regplace, p1_fname, p1_lname, p2_fname, p2_lname):
     insertions = (regno, regdate, regplace, p1_fname, p2_lname, p2_fname, p2_lname)
     c.execute('INSERT INTO marriages VALUES(?,?,?,?,?,?,?);', insertions)
-    conn.commit()
 
 def create_registration(regno, regdate, expiry, plate, vin, fname, lname):
     insertions = (regno, regdate, expiry, plate, vin, fname, lname)
     c.execute('INSERT INTO registrations VALUES(?,?,?,?,?,?,?);', insertions)
-    conn.commit()
 
 def create_payment(tno, pdate, amount):
     insertions = (tno, pdate, amount)
     c.execute("INSERT INTO payments VALUES(?,?,?)", insertions)
-    conn.commit()
 
 def get_city_of_user(uid):
     c.execute('''SELECT city
